7a601c650b1c9597737828e9de440d7f401818a5
Yannick Musafiri <yannick@musafiri.co>
2026-03-08 13:12:04 +0100

    chore: add dark icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,14 +13,34 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: THEME_COLORS.dark,
     icons: [
       {
+        src: '/icon.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+        purpose: 'any',
+      },
+      {
         src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any',
       },
       {
         src: '/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/icon-maskable-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon-maskable-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
